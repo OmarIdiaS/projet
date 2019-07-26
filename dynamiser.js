bc7b5c7489fbafae32172a20f4012d88fe341700
OmarIdiaS <34215927+OmarIdiaS@users.noreply.github.com>
2019-07-26 05:14:28 -0700

    Add files via upload

---
+++ dynamiser.js
@@ -0,0 +1,14 @@
+// Wrap every letter in a span
+  
+
+  window.onscroll = function(){
+    ScrollFunction();
+  }
+  function ScrollFunction(){
+    document.getElementById("home").classList.add('prj');
+    
+
+
+  }
+
+  
